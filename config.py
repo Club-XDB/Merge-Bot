f9f4c3b23ce7eb4a2c39cf6f03f41eb23ff9494b
Skkmsinkisjnnj <120069390+Skkmsinkisjnnj@users.noreply.github.com>
2022-12-18 01:05:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
+    API_HASH = "6136c4d25def575624a2baa83d022826"
+    BOT_TOKEN = "5665068184:AAHVy9pPeIsMnd7cSXcaq4X3OuKYggY2ydQ"
+    TELEGRAM_API = "27168211"
+    OWNER = "5674750841"
+    OWNER_USERNAME = "@Ayankojiko"
+    PASSWORD = "12345"
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = "-1001871188394"
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
